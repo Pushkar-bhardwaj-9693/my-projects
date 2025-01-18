d16bb6124ac71f06fac1e723bf9c85174e718af1
Pushkar-bhardwaj-9693 <pankaj8521539022@gmail.com>
2025-01-18 17:33:05 +0530

    updated readme

--- square calculator/script.js
+++ square calculator/script.js
@@ -1,4 +1,4 @@
-let nums = [prompt("Enter a number"), prompt("Enter a number"), prompt("Enter a number")];
+let nums = [prompt("Enter a number")];
 
 nums.forEach((num) => {
     console.log(num*num);
